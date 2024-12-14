c3ded92472a9226aeb62ffb476467f9aeb7d52af
Your Name <you@example.com>
2024-12-15 03:26:41 +0530

    Modal,Entity,PackageInstallation

---
+++ DTO/BaseDto/TimeEntityDto.cs
@@ -0,0 +1,13 @@
+﻿namespace STUDMANAG.DTO.BaseDto
+{
+    public class TimeEntityDto
+    {
+        public int Id { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
+        public string CreatedId { get; set; }
+        public string DeletedId { get; set; }
+        public DateTime? LastModifiedTime { get; set;}
+        public string LastModifiedId { get; set; }
+    }
+}
---
+++ Modal/AspUsers.cs
@@ -0,0 +1,13 @@
+﻿using STUDMANAG.Modal.Basemodal;
+
+namespace STUDMANAG.Modal
+{
+    public class AspUsers : TimeEntity
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public int? RoleId   { get; set; }
+
+    }
+}
