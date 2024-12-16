7755bca6ea5ace273606e150aa2aecd7072b7341
Your Name <you@example.com>
2024-12-16 12:38:55 +0530

    STDto

---
+++ DTO/StudentDto.cs
@@ -0,0 +1,17 @@
+﻿using STUDMANAG.DTO.BaseDto;
+
+namespace STUDMANAG.DTO
+{
+    public class StudentDto : TimeEntityDto
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public int RollNo { get; set; }
+        public string AdmissionNumber { get; set; }
+        public DateTime DOB { get; set; }
+        public int Gender { get; set; }
+        public string Address { get; set; }
+
+    }
+}
