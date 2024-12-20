c4dcf2deb81eb2352a678b3ed1ad7fc176ef1188
ShanVaales <shanlal.k@vaales.com>
2024-12-20 20:46:35 +0530

    DbContextModal Registration

--- SContext/SDbcontext.cs
+++ SContext/SDbcontext.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using STUDMANAG.Modal;
 
 namespace STUDMANAG.SContext
 {
     public class SDbcontext : DbContext
     {
         public SDbcontext(DbContextOptions<SDbcontext> options) : base(options) { }
-        //public DbSet
+        public DbSet<AspRoles> AspRoles { get; set; }
+        public DbSet<AspUserRoleDetailsEntity> AspUserRoleDetails { get; set; }
+        public DbSet<AspUsers> AspUsers { get; set; }
+        public DbSet<ClassDetailsEntity> ClassDetails { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<ExamDetails> ExamsDetails { get; set; }
+        public DbSet<PrincipalEntity> Principals { get; set; }
+        public DbSet<SchoolEntity> Schools { get; set; }
+        public DbSet<StudentAcademicDetail> StudentAcademicDetails { get; set; }
     }
 }
