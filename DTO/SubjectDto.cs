5829cf6790a54b5897c2eb98c77e47a5115da9f7
ShanVaales <shanlal.k@vaales.com>
2024-12-20 20:36:09 +0530

    Modal Creation Complete

---
+++ DTO/SubjectDto.cs
@@ -0,0 +1,11 @@
+﻿using STUDMANAG.DTO.BaseDto;
+
+namespace STUDMANAG.DTO
+{
+    public class SubjectDto : TimeEntityDto
+    {
+        public string SubjectName { get; set; }
+        public string? SubjectCode { get; set; }
+        public string? SubjectDescription { get; set; }
+    }
+}
---
+++ DTO/TeacherSubjectDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using STUDMANAG.DTO.BaseDto;
+
+namespace STUDMANAG.DTO
+{
+    public class TeacherSubjectDetailsDto : TimeEntityDto
+    {
+        public int SubjectId { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
